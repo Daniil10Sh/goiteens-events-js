46e08694d6c0ed8530087ad9dccf0ed3a2112dba
Daniil10Sh <danik2022odessa@gmail.com>
2025-05-27 18:34:56 +0300

    changes

---
+++ src/components/Event.jsx
@@ -0,0 +1,10 @@
+export default function Event() {
+    return (
+        <div className="name">
+            <h2>Event name</h2>
+            
+
+        </div>
+    )
+
+}
--- src/components/PageBoard.jsx
+++ src/components/PageBoard.jsx
@@ -1,13 +1,15 @@
-import Data from '../upcoming-events.json';
+import Data from "../upcoming-events.json"
+import Event from "./Events"
 
-export default function PageBoard({events = Data}) {
-    return (
+export default function PageBoard({events = Data}){
+    return(
         <>
-        <h1>Hi</h1>
+        <h1>hi</h1>
 
         {events.map((event, index) => (
-            <Event key={index} name={event.name}> </Event>
+            <Event key={index} name={event.name}></Event>
         ))}
+        
         </>
-    );
+    )
 }
