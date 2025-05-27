8458d59056e55964059fb09feee20da8b3ae39d9
Daniil10Sh <danik2022odessa@gmail.com>
2025-05-27 19:40:41 +0300

    new changes

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,9 +1,12 @@
-export default function Event() {
+export default function Event({name, location, speaker,type,start,end}) {
     return (
-        <div className="name">
-            <h2>Event name</h2>
-            
-
+        <div className="event-container">
+            <h2 className="event-name">{name}</h2>
+            <p className="event-location">Location: {location}</p>
+            <p className="event-speaker">Speaker: {speaker}</p>
+            <p className="event-type">Type: {type}</p>
+            <p className="event-start">Start: {start}</p>
+            <p className="event-end">End: {end}</p>
         </div>
     )
 
